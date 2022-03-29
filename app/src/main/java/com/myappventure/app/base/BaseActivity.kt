bfb5386165a2f8eeb9e7ad66bab260b44ff5fe12
Shintaapz <shinta.amirul@gmail.com>
2022-03-29 20:52:36 +0700

    Basse (BaseActivity,BaseViewModel,Massage)

--- app/src/main/java/com/myappventure/app/base/BaseActivity.kt
+++ app/src/main/java/com/myappventure/app/base/BaseActivity.kt
@@ -1,6 +1,76 @@
 package com.myappventure.app.base
 
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.myappventure.app.BuildConfig
+import com.myappventure.app.dialog.CustomLoadingDialog
 
 abstract class BaseActivity : AppCompatActivity() {
+
+    private var isAlertShow = false
+    private lateinit var loadingUI: CustomLoadingDialog
+
+    override fun onStart() {
+        super.onStart()
+        if (BuildConfig.DEBUG) {
+            window?.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+        loadingUI = CustomLoadingDialog(this)
+
+        setupObserver()
+    }
+
+    protected fun showLoading() {
+        loadingUI.show()
+    }
+
+    protected fun hideLoading() {
+        loadingUI.hide()
+    }
+
+    protected fun showMessage(message: Message) {
+        when (message) {
+            is Message.Toast -> showMessageToast(message.message)
+            is Message.LongToast -> showMessageLongToast(message.message)
+            is Message.Dialog -> showMessageDialog(message.message)
+        }
+    }
+
+    protected fun showMessageDialog(msg: String?) {
+        msg?.let {
+            if (!isAlertShow) {
+                AlertDialog.Builder(this)
+                    .setMessage(it)
+                    .setCancelable(false)
+                    .setPositiveButton("OK") { dialog, _ ->
+                        isAlertShow = false
+                        dialog.dismiss()
+                    }.show()
+                isAlertShow = true
+            }
+        }
+    }
+
+    protected fun showMessageToast(msg: String?) {
+        msg?.let {
+            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    protected fun showMessageLongToast(msg: String?) {
+        msg?.let {
+            Toast.makeText(this, it, Toast.LENGTH_LONG).show()
+        }
+    }
+
+    protected abstract fun setupObserver()
 }
--- app/src/main/java/com/myappventure/app/base/Message.kt
+++ app/src/main/java/com/myappventure/app/base/Message.kt
@@ -1,4 +1,7 @@
 package com.myappventure.app.base
 
 sealed class Message {
+    class Toast(val message: String?) : Message()
+    class LongToast(val message: String?) : Message()
+    class Dialog(val message: String?) : Message()
 }
