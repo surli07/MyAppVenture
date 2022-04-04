9e12310a4176974aef2b47ada136ffacd2e1f663
surli07 <surli07>
2022-04-04 15:11:00 +0700

    Add success landing xml

---
+++ app/src/main/java/com/myappventure/app/ui/SuccessLandingActivity.kt
@@ -0,0 +1,12 @@
+package com.myappventure.app.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.myappventure.app.R
+
+class SuccessLandingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_success_landing)
+    }
+}
