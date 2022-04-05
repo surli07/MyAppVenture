fdb08b242793583ed267b9b00c3fcc0c5d26a7a0
Shintaapz <shinta.amirul@gmail.com>
2022-04-05 22:35:43 +0700

    revisi NetworkModule

--- app/src/main/java/com/myappventure/app/di/NetworkModule.kt
+++ app/src/main/java/com/myappventure/app/di/NetworkModule.kt
@@ -1,4 +1,29 @@
 package com.myappventure.app.di
 
+import android.app.Application
+import com.myappventure.app.data.remote.ApiClient
+import com.myappventure.app.data.remote.ApiService
+import com.myappventure.app.data.remote.NetworkStateManager
+import com.myappventure.app.data.remote.NetworkStateManagerImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideApiService(): ApiService {
+        return ApiClient.instance
+    }
+
+    @Provides
+    @Singleton
+    fun provideNetworkStateManager(application: Application): NetworkStateManager {
+        return NetworkStateManagerImpl(application)
+    }
 }
