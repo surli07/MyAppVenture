67ffa817a82ae54624e08a2fbdf8d434a4969006
Shintaapz <shinta.amirul@gmail.com>
2022-04-06 22:25:25 +0700

    implementasi login api

--- app/src/main/java/com/myappventure/app/ui/login/LoginActivity.kt
+++ app/src/main/java/com/myappventure/app/ui/login/LoginActivity.kt
@@ -1,20 +1,51 @@
 package com.myappventure.app.ui.login
 
 import android.os.Bundle
+import android.os.Message
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.myappventure.app.R
+import androidx.lifecycle.lifecycleScope
+import com.myappventure.app.base.BaseActivity
 import com.myappventure.app.databinding.ActivityLoginBinding
-import com.myappventure.app.databinding.ActivityMainBinding
-import com.myappventure.app.ui.register.RegisterViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
-class LoginActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class LoginActivity : BaseActivity() {
 
     private lateinit var binding: ActivityLoginBinding
     private val viewModel: LoginViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnMasuk.setOnClickListener {
+            val email = binding.email.text.toString()
+            val password = binding.password.text.toString()
+            when {
+                email.isEmpty() -> {
+                    //TODO SHOW ERROR EMAIL KOSONG
+                }
+                password.isEmpty() -> {
+                    //TODO SHOW ERROR PASS KOSONG
+                }
+                else -> {
+                    lifecycleScope.launch {
+                        viewModel.startLogin(email, password)
+                    }
+                }
+            }
+        }
+    }
+
+    override fun setupObserver() {
+        viewModel.message.observe(this) {
+            //TODO SHOW LINEAR LAYOUT DAN UBAH TEXTVIEW
+        }
+        viewModel.loading.observe(this) {
+            if (it) showLoading() else hideLoading()
+        }
     }
 }
--- app/src/main/java/com/myappventure/app/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/myappventure/app/ui/login/LoginViewModel.kt
@@ -1,6 +1,7 @@
 package com.myappventure.app.ui.login
 
 import com.myappventure.app.base.BaseViewModel
+import com.myappventure.app.data.remote.login.LoginBody
 import com.myappventure.app.repository.AuthRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -9,5 +10,18 @@ import javax.inject.Inject
 class LoginViewModel @Inject constructor(
     private val authRepository: AuthRepository
 ) : BaseViewModel() {
+    suspend fun startLogin(email: String, password: String) {
+        val loginBody = LoginBody(email, password)
+        authRepository.loginUser(
+            onStart = {
+            },
+            onComplete = {
+            },
+            onError = {
+            },
+            loginBody
+//TODO LIHAT DI BIN MOVIE MAIN VIEW MODEL simpan di livedata
+        )
+    }
 
 }
