109faa4873f137ec0d6d42913522df925ac6e522
Shintaapz <shinta.amirul@gmail.com>
2022-04-18 23:48:09 +0700

    benerin fragment profile

--- app/src/main/java/com/myappventure/app/ui/navigation/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/myappventure/app/ui/navigation/ui/profile/ProfileFragment.kt
@@ -1,15 +1,19 @@
 package com.myappventure.app.ui.navigation.ui.profile
 
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.google.android.material.tabs.TabLayoutMediator
-import com.myappventure.app.databinding.FragmentMengikutiBinding
-import com.myappventure.app.databinding.FragmentPengikutBinding
+import com.myappventure.app.databinding.ActivityProfileBinding
 import com.myappventure.app.databinding.FragmentProfileBinding
+import com.myappventure.app.ui.MainFollowActivity
+import com.myappventure.app.ui.login.LoginActivity
+import com.myappventure.app.ui.navigation.ui.mengikuti.MengikutiFragment
+import com.myappventure.app.ui.navigation.ui.pengikut.PengikutFragment
 import com.myappventure.app.ui.register.RegisterActivity
 
 class ProfileFragment : Fragment() {
@@ -27,33 +31,18 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.viewPager.apply {
-            this.adapter = ViewPagerAdapter2(this@ProfileFragment)
+        binding.txtPengikut.setOnClickListener {
+            val intent = Intent(requireContext(), MainFollowActivity::class.java)
+            startActivity(intent)
         }
-
-        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
-            tab.text = when (position) {
-                0 -> "Pengikut"
-                else -> "Mengikuti"
-            }
-        }.attach()
-
-        binding.txtMengikuti.setOnClickListener { showTab(true) }
-        binding.txtPengikut.setOnClickListener { showTab(true) }
+        binding.txtMengikuti.setOnClickListener {
+            val intent = Intent(requireContext(), MainFollowActivity                                                          ::class.java)
+            startActivity(intent)
+        }
         binding.edtFoto.setOnClickListener {
         }
         setupObserver()
 
-    }
-
-    private fun showTab(isShow: Boolean) {
-        if (isShow) {
-            binding.linearLayout.visibility = View.VISIBLE
-            binding.viewPager.visibility = View.VISIBLE
-        } else {
-            binding.linearLayout.visibility = View.GONE
-            binding.viewPager.visibility = View.GONE
-        }
     }
 
     private fun setupObserver() {
