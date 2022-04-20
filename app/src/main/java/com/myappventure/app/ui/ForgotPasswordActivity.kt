1eef98e8dd7b32029c3a5dc4256ac2f7142910ab
Shintaapz <shinta.amirul@gmail.com>
2022-04-20 21:14:56 +0700

    forgot pass

--- app/src/main/java/com/myappventure/app/ui/ForgotPasswordActivity.kt
+++ app/src/main/java/com/myappventure/app/ui/ForgotPasswordActivity.kt
@@ -1,12 +1,30 @@
 package com.myappventure.app.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
 import com.myappventure.app.R
+import com.myappventure.app.databinding.ActivityForgotPasswordBinding
+import com.myappventure.app.databinding.ActivityLoginBinding
+import com.myappventure.app.ui.register.RegisterActivity
+import kotlinx.coroutines.launch
 
 class ForgotPasswordActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityForgotPasswordBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_forgot_password)
+
+        binding = ActivityForgotPasswordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnKirim.setOnClickListener {
+            val i = Intent(this, KodeOtpActivity::class.java)
+            startActivity(i)
+            finish()
+        }
     }
 }
