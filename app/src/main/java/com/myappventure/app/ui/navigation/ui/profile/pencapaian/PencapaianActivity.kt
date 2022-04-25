3213cb07fc388b0763eddcdba9ac190125a3fbde
Shintaapz <shinta.amirul@gmail.com>
2022-04-26 00:16:31 +0700

    profile_profile_komunitas_pencapaian

---
+++ app/src/main/java/com/myappventure/app/ui/navigation/ui/profile/pencapaian/PencapaianActivity.kt
@@ -0,0 +1,12 @@
+package com.myappventure.app.ui.navigation.ui.profile.pencapaian
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.myappventure.app.R
+
+class PencapaianActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pencapaian)
+    }
+}
