a530f5e29cfab6bec83e3f7ee32875d6c6793a40
surli07 <surli07>
2022-04-26 13:33:40 +0700

    Add postingan

---
+++ app/src/main/java/com/myappventure/app/ui/navigation/ui/home/create_postingan/CreatePostinganActivity.kt
@@ -0,0 +1,12 @@
+package com.myappventure.app.ui.navigation.ui.home.create_postingan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.myappventure.app.R
+
+class CreatePostinganActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_postingan)
+    }
+}
