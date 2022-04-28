dc0e9dbd33b52b1966b39f8ca2ef2b95f7851427
surli07 <surli07>
2022-04-28 18:29:12 +0700

    Upload image di register

--- app/src/main/java/com/myappventure/app/data/remote/register/Data.kt
+++ app/src/main/java/com/myappventure/app/data/remote/register/Data.kt
@@ -4,20 +4,10 @@ package com.myappventure.app.data.remote.register
 import com.google.gson.annotations.SerializedName
 
 data class Data(
-    @SerializedName("authorities")
-    val authorities: List<Authority>,
-    @SerializedName("fullname")
-    val fullname: String,
-    @SerializedName("id")
-    val id: Int,
-    @SerializedName("nama")
-    val nama: String,
-    @SerializedName("otp")
-    val otp: String,
-    @SerializedName("otpExpiredDate")
-    val otpExpiredDate: String,
-    @SerializedName("roles")
-    val roles: List<Role>,
-    @SerializedName("username")
-    val username: String
+    @SerializedName("data")
+    val `data`: DataX,
+    @SerializedName("message")
+    val message: String,
+    @SerializedName("status")
+    val status: String
 )
--- app/src/main/java/com/myappventure/app/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/myappventure/app/ui/register/RegisterActivity.kt
@@ -1,9 +1,18 @@
 package com.myappventure.app.ui.register
 
+import android.Manifest
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.widget.doAfterTextChanged
 import androidx.lifecycle.lifecycleScope
@@ -16,13 +25,37 @@ import com.myappventure.app.ui.navigation.NavigationActivity
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import org.w3c.dom.Text
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 import java.util.regex.Pattern
+import kotlin.math.roundToInt
+import kotlin.math.sqrt
 
 @AndroidEntryPoint
 class RegisterActivity : BaseActivity() {
 
     private lateinit var binding: ActivityRegisterBinding
     private val viewModel: RegisterViewModel by viewModels()
+    private val requestGetPhoto = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val selectedUri: Uri? = result.data?.data
+            selectedUri?.let { uri ->
+                binding.imgPhotoUser.setImageURI(uri)
+                binding.imgPhotoUser.visibility = View.VISIBLE
+                createFileBeforeUpload(uri)
+            }
+        } else {
+            binding.imgPhotoUser.visibility = View.GONE
+            Toast.makeText(
+                this,
+                "Pemilihan gambar dibatalkan",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
     private val emailPattern = Pattern.compile(
         "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
                 "\\@" +
@@ -34,6 +67,7 @@ class RegisterActivity : BaseActivity() {
     )
     private val usernameRegex = Pattern.compile("[a-z]{3,15}")
     private val passwordREGEX = Pattern.compile("(?=\\S+$).{6,}$")
+    var file = File("")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,6 +75,9 @@ class RegisterActivity : BaseActivity() {
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.cardPenUpload.setOnClickListener {
+            requestAccessForFile()
+        }
         binding.btnRegister.setOnClickListener {
             register()
         }
@@ -75,6 +112,87 @@ class RegisterActivity : BaseActivity() {
         setupObserver()
     }
 
+    private fun requestAccessForFile() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                777
+            )
+        } else {
+            selectFileForUpload()
+        }
+
+    }
+
+    private fun selectFileForUpload() {
+        val i = Intent().apply {
+            type = "image/*"
+            action = Intent.ACTION_GET_CONTENT
+        }
+        requestGetPhoto.launch(Intent.createChooser(i, "Select Picture"))
+    }
+
+    private fun createFileBeforeUpload(uri: Uri) {
+        lifecycleScope.launch {
+            kotlin.runCatching {
+                try {
+                    val targetOriginal =
+                        BitmapFactory.decodeStream(
+                            this@RegisterActivity.contentResolver.openInputStream(
+                                uri
+                            )
+                        )
+                    val targetCompressed = reduceBitmapSize(targetOriginal)
+                    val bos = ByteArrayOutputStream()
+                    targetCompressed.compress(Bitmap.CompressFormat.JPEG, 0, bos)
+                    val bitmapData = bos.toByteArray()
+
+                    file = File(
+                        this@RegisterActivity.filesDir.path,
+                        "${System.currentTimeMillis()}_compressed.jpeg"
+                    )
+                    file.createNewFile()
+                    val fos = FileOutputStream(file)
+                    fos.write(bitmapData)
+                    fos.flush()
+                    fos.close()
+
+                    // lempar file ke view model untuk diupload retrofit
+
+                } catch (e: Exception) {
+                    this@RegisterActivity.runOnUiThread {
+                        Toast.makeText(
+                            this@RegisterActivity,
+                            "Terjadi kesalahan saat menulis file",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun getFile(file: File) : File{
+        return file
+    }
+
+    private fun reduceBitmapSize(bitmap: Bitmap, MAX_SIZE: Int = 360000): Bitmap {
+        val ratioSquare: Double
+        val bitmapHeight: Int = bitmap.height
+        val bitmapWidth: Int = bitmap.width
+        ratioSquare = (bitmapHeight * bitmapWidth / MAX_SIZE).toDouble()
+        if (ratioSquare <= 1) return bitmap
+        val ratio = sqrt(ratioSquare)
+        val requiredHeight = (bitmapHeight / ratio).roundToInt()
+        val requiredWidth = (bitmapWidth / ratio).roundToInt()
+        return Bitmap.createScaledBitmap(bitmap, requiredWidth, requiredHeight, true)
+    }
+
     override fun setupObserver() {
         val loadingUi = CustomLoadingDialog(this)
         viewModel.loading.observe(this) {
@@ -84,7 +202,7 @@ class RegisterActivity : BaseActivity() {
             showMessageToast(it)
         }
         viewModel.registerResponse.observe(this){
-            val i = Intent(this, SuksesRegisterActivity::class.java)
+            val i = Intent(this@RegisterActivity, SuksesRegisterActivity::class.java)
             startActivity(i)
             finish()
         }
@@ -126,13 +244,11 @@ class RegisterActivity : BaseActivity() {
         var email = checkEmail()
         var password = checkPasssword()
 
-        if (username && email && password) {
-            binding.btnRegister.isEnabled = true
+
+            binding.btnRegister.isEnabled = username && email && password
             binding.btnRegister.setTextColor(ContextCompat.getColor(applicationContext, R.color.white))
-        } else {
-            binding.btnRegister.isEnabled = false
             binding.btnRegister.setTextColor(ContextCompat.getColor(applicationContext, R.color.green))
-        }
+
     }
 
     private fun register() {
@@ -140,11 +256,9 @@ class RegisterActivity : BaseActivity() {
         val username = binding.edtUsername.text.toString()
         val password = binding.edtPassword.text.toString()
 
-
         lifecycleScope.launch {
-            viewModel.startRegister(email, username, password)
+            viewModel.startRegister(file, email, username, password)
             setupObserver()
-
         }
     }
 }
