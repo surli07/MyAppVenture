e597c516b0b7286a0e4644e0038cc2f568ba735a
Shintaapz <shinta.amirul@gmail.com>
2022-04-28 13:18:52 +0700

    detail destinasi_detail komunitas

---
+++ app/src/main/java/com/myappventure/app/ui/navigation/ui/komunitas/DetailKomunitasActivity.kt
@@ -0,0 +1,12 @@
+package com.myappventure.app.ui.navigation.ui.komunitas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.myappventure.app.R
+
+class DetailKomunitasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_komunitas)
+    }
+}
