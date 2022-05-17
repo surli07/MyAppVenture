f391aea7d328ffad923295f5ba1a4eef3f0ed620
Shintaapz <shinta.amirul@gmail.com>
2022-05-17 16:06:53 +0700

    create komunitas

--- app/src/main/java/com/myappventure/app/ui/navigation/ui/komunitas/KomunitasFragment.kt
+++ app/src/main/java/com/myappventure/app/ui/navigation/ui/komunitas/KomunitasFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.myappventure.app.data.local.MySharedPref
 import com.myappventure.app.databinding.FragmentKomunitasBinding
 import com.myappventure.app.ui.navigation.ui.community.NewCommunityActivity
 import dagger.hilt.android.AndroidEntryPoint
@@ -27,6 +28,8 @@ class KomunitasFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        if (MySharedPref.isLoggedIn) binding.imgCreate.visibility = View.VISIBLE
 
         binding.imgCreate.setOnClickListener {
             val intent = Intent(requireContext(), NewCommunityActivity::class.java)
